4311a752ce99655413bc1e49617223f8b0576ee2
Branimir Kolev <kolevb780@gmail.com>
2024-04-24 10:38:59 +0300

    tried to print out the name with the prediction

--- code.py
+++ code.py
@@ -1,16 +1,20 @@
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 
 flags = pd.read_csv('flag.csv', header=0)
 data = flags[["Red", "Green", "Blue", "Gold", "White", "Black", "Orange", "Circles", "Crosses","Saltires","Quarters","Sunstars", "Crescent","Triangle"]]
-labels = flags[['Name']]
-
-data_train, data_test, labels_train, labels_test=train_test_split(data, labels, random_state=1, test_size=0.2)
+labels = flags[['Landmass']]
 
 classifier = DecisionTreeClassifier(max_depth=5, random_state=1)
-classifier.fit(data_train, labels_train)
+classifier.fit(data, labels)
 
+predicted_probabilities = classifier.predict_proba([[1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
-print(classifier.score(data_test, labels_test))
+top_indices = np.argpartition(predicted_probabilities, -3, axis=1)[:, -3:]
+
+top_countries = [[flags.iloc[index]['Name'] for index in indices] for indices in top_indices]
+
+print("Top 3 countries:", top_countries)
+predicted_point=classifier.predict([[1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
  # type: ignore
