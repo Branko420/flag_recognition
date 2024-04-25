51bfbc546777dc708d359e26eb28826ddca78677
Branimir Kolev <kolevb780@gmail.com>
2024-04-25 14:04:52 +0300

    make it to give me the top 3 predicted countries

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 
@@ -13,9 +14,11 @@ labels = flags[['Name']]
 classifier = DecisionTreeClassifier(max_depth=194, random_state=1)
 classifier.fit(data, labels)
 
-new_data_point = [3,5,1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1,0]
+new_data_point = [[3,5,1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1,0]]
 
-predicted_country = classifier.predict([new_data_point])
+predicted_country = classifier.predict_proba(new_data_point)
+top_indices =  np.argpartition(predicted_country, -3, axis=1)[:,-3:]
+top_three_countries = [[flags.iloc[i]["Name"] for i in indice] for indice in top_indices]
 
-print("Predicted country:", predicted_country)
+print("Predicted country:", top_three_countries[0][::-1])
 #type:ignore
