d4139f7e4c58d0dc307a18d4c62e494775b1fad4
Branimir Kolev <kolevb780@gmail.com>
2024-04-25 13:38:48 +0300

    added some new features into data and changed max_depth to 194

--- code.py
+++ code.py
@@ -1,20 +1,21 @@
-from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
-import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
 
 flags = pd.read_csv('flag.csv', header=0)
-data = flags[["Red", "Green", "Blue", "Gold", "White", "Black", "Orange", "Circles", "Crosses","Saltires","Quarters","Sunstars", "Crescent","Triangle"]]
-labels = flags[['Landmass']]
+flags = flags.dropna()
 
-classifier = DecisionTreeClassifier(max_depth=5, random_state=1)
+data = flags[['Stripes','Colors',"Red", "Green", "Blue", "Gold", "White", "Black", "Orange", "Circles", "Crosses","Saltires","Quarters","Sunstars", "Crescent","Triangle", "Animate", "Icon", "Text"]]
+labels = flags[['Name']]
+
+classifier = DecisionTreeClassifier(max_depth=194, random_state=1)
 classifier.fit(data, labels)
 
-predicted_probabilities = classifier.predict_proba([[1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+new_data_point = [3,5,1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1,0]
 
-top_indices = np.argpartition(predicted_probabilities, -3, axis=1)[:, -3:]
+predicted_country = classifier.predict([new_data_point])
 
-top_countries = [[flags.iloc[index]['Name'] for index in indices] for indices in top_indices]
-
-print("Top 3 countries:", top_countries)
-predicted_point=classifier.predict([[1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
- # type: ignore
+print("Predicted country:", predicted_country)
+#type:ignore
