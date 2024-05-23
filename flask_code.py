ec0323c05d893f5d8bbd6253a8b1bd3b48431c66
Branimir Kolev <kolevb780@gmail.com>
2024-05-23 11:37:01 +0300

    trying to deploy with railway

--- flask_code.py
+++ flask_code.py
@@ -29,5 +29,5 @@ def predict():
         return render_template('predicted_flag_page.html', prediction = predicted_coutry )
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000, debug=True)
 #type:ignore
